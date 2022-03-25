38b2121ca82c1abb084852e519babbc0827bc4bc
Ivanildo Teles <itsantos@icloud.com>
2022-03-24 22:05:32 -0300

    Baskara v1

--- Baskara.java
+++ Baskara.java
@@ -22,11 +22,16 @@ public class Baskara{
         double delta = Math.pow(b, 2) - (4 * a * (c));
         System.out.println("Delta = " + delta);
 
-        double x1 = (-b + Math.sqrt(delta)) / (2 * a);
-        System.out.println("X1 = " + x1);
+        if(delta < 0 || a == 0) {
+            System.out.println("Impossivel calcular!");
+        }else{
 
-        double x2 = (-b - Math.sqrt(delta)) / (2 * a);
-        System.out.println("X2 = " + x2);
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            System.out.println("X1 = " + x1);
+            
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("X2 = " + x2);
+        }
 
         entrada.close();
     }
